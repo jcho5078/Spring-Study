95158bf93fc1c6988dc36c3e0a2c8b339a326bf1
jcho5078 <60742556+jcho5078@users.noreply.github.com>
2020-08-09 18:25:16 +0900

    study spring mvc

--- SpringMVC/src/main/java/com/jcho5078/mvc/validate/annotation/NoValidatorClass.java
+++ SpringMVC/src/main/java/com/jcho5078/mvc/validate/annotation/NoValidatorClass.java
@@ -1,10 +1,10 @@
 package com.jcho5078.mvc.validate.annotation;
 
-import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.annotations.NotFound;
 
 public class NoValidatorClass {
 	
-	@NotEmpty  //Validator 클래스를 따로 정의하고 싶지 않다면, 규칙을 정의한 JSR303 Validator을 DI로 설정해 글로번 Validator으로 사용. 
+	@NotFound //Validator 클래스를 따로 정의하고 싶지 않다면, 규칙을 정의한 JSR303 Validator을 DI로 설정해 글로번 Validator으로 사용. 
 	private String name;
 
 }
--- SpringMVC/src/main/webapp/WEB-INF/views/home.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/views/home.jsp
@@ -19,5 +19,6 @@ ${session}
 <a href="board/0">board 0 page 이동</a><br>
 <a href="board/1">board 1 page 이동</a><br>
 <a href="eventForm">뷰 => 컨트롤러에 값 전송 후 받아오기</a><br>
+<a href="sessionForm2">세션유지 확인하기</a><br>
 </body>
 </html>
